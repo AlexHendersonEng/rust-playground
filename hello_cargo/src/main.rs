0324f5bf6e1e77dc2cc4e7595bb51cdb378eea08
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-02-02 10:17:45 +0000

    Added hello_cargo project

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,17 @@
+/*
+    This project was generated using Cargo which is Rust's build system and package manager.
+
+    Using 'cargo new' generates a project directory with a 'src' directory and a 'Cargo.toml'
+    file which is a configuration file that describes how to build the project.
+
+    When building a project using 'cargo build' a Cargo.lock file is generated which keeps
+    track of the exact versions of dependencies in the project.
+
+    Using 'cargo run' will build and run the project.
+
+    Using 'cargo check' will check the project for errors but will not generate an executable.
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
